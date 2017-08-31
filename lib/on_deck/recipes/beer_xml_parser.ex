7ed491582460200918e98c35b35b46a1427812b7
Chase <chase.gilliam@gmail.com>
2017-08-31 01:01:24 -0400

    added fermentables and yeasts to parser

--- lib/on_deck/recipes/beer_xml_parser.ex
+++ lib/on_deck/recipes/beer_xml_parser.ex
@@ -17,16 +17,15 @@ defmodule OnDeck.Recipes.BeerXmlParser do
   def parse(xml_string) do
     xml_string
       |> build_ingredients_list
-  end
+  end 
   def build_ingredients_list(xml_string) do
-
     try do
       {:ok,
         %{
           meta: %{},
           hops: get_hops(xml_string),
-          fermentables: [%{}],
-          yeasts: [%{}],
+          fermentables: get_fermentables(xml_string),
+          yeasts: get_yeasts(xml_string),
           misc: [%{},
           equipment: %{},
           mash: %{}]
@@ -48,5 +47,34 @@ defmodule OnDeck.Recipes.BeerXmlParser do
         hop_use: ~x"USE/text()",
         time: ~x"TIME/text()",
         display_time: ~x"DISPLAY_TIME/text()")
+  end
+
+  def get_fermentables(xml) do
+    xml 
+    |> xpath(~x"/RECIPES/RECIPE/FERMENTABLES/FERMENTABLE"l,
+        name: ~x"NAME/text()",
+        type: ~x"TYPE/text()",
+        ammount: ~x"AMMOUNT/text()",
+        yield: ~x"YIELD/text()",
+        color: ~x"COLOR/text()",
+        notes: ~x"NOTES/text()",
+        moisture: ~x"MOISTURE/text()"
+    )
+  end
+
+  def get_yeasts(xml) do
+    xml 
+      |> xpath(~x"/RECIPES/RECIPE/YEASTS/YEAST"l,
+          name: ~x"NAME/text()",
+          type: ~x"TYPE/text()",
+          ammount: ~x"AMMOUNT/text()",
+          laboratory: ~x"LABORATORY/text()",
+          min_temperature: ~x"MIN_TEMPERATURE/text()",
+          max_temperature: ~x"MAX_TEMPERATURE/text()",
+          flocculation: ~x"FLOCCULATION/text()",
+          attenuation: ~x"ATTENUATION/text()",
+          notes: ~x"NOTES/text()",
+          best_for: ~x"BEST_FOR/text()"
+      )
   end
 end
