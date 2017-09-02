b02ddccc2e75710f8480aeb6eb33f14d77b3c488
Chase <chase.gilliam@gmail.com>
2017-09-02 16:35:26 -0400

    minimal graphql search for user and their beers

--- deck-js/src/routes/profile/index.js
+++ deck-js/src/routes/profile/index.js
@@ -5,7 +5,9 @@ import style from './style';
 export default class Profile extends Component {
 	state = {
 		time: Date.now(),
-		count: 10
+		count: 10,
+		userName: "",
+		beerCount: 0,
 	};
 
 	increment = () => {
@@ -16,11 +18,62 @@ export default class Profile extends Component {
 	updateTime = () => {
 		this.setState({ time: Date.now() });
 	};
+
+	parseData = () => {
+		const user = this.state.data.user
+		this.setState({
+			userName: user.name,
+			beerCount: user.beers.length
+		});
+	}
 	
 	// gets called when this route is navigated to
 	componentDidMount() {
-		const uri = 'http://api.githunt.com/graphql';
+		const uri = 'http://localhost:4000/graph';
 		const apolloFetch = createApolloFetch({ uri });
+
+		const query = `
+			query userBeers($id: ID!, $onTap: Boolean) {
+				user(id: $id){
+					name
+					id
+					uuid
+					beers(onTap: $onTap){
+						id
+						name
+						style
+						description
+						ingredients
+						abv
+						measuredOriginalGravity
+						measuredFinalGravity
+						boilTime
+						estimatedIbu
+						volumeInLiters
+						brewDate
+						bottleOrKegDate
+						rating
+						onTap
+					}
+				}
+			}
+		`;
+		
+		const variables = {
+			id: this.props.user,
+			onTap: true,
+		};
+
+		apolloFetch({ query, variables })
+			.then(result => {
+				const { data, errors, extensions } = result;
+				this.setState({data: data});
+				this.parseData();
+			})
+			.catch(error => {
+				console.log(error)
+				//respond to a network error
+		});
 		// start a timer for the clock:
 		this.timer = setInterval(this.updateTime, 1000);
 	}
@@ -31,14 +84,14 @@ export default class Profile extends Component {
 	}
 
 	// Note: `user` comes from the URL, courtesy of our router
-	render({ user }, { time, count }) {
+	render({ user }, { time, count, userName, beerCount }) {
 		return (
 			<div class={style.profile}>
-				<h1>Profile: {user}</h1>
-				<p>This is the user profile for a user named { user }.</p>
+				<h1>Profile: {userName}</h1>
+				<p>This is the user profile for a user named {userName}.</p>
 
 				<div>Current time: {new Date(time).toLocaleString()}</div>
-
+				<div>beers on tap: {beerCount}</div>
 				<p>
 					<button onClick={this.increment}>Click Me</button>
 					{' '}
